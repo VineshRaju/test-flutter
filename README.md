d6a92213b065143fa22d26635049ab11cd53f530
VineshRaju <vineshraju@rocketmail.com>
2017-12-04 04:41:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# test_flutter
+
+A new Flutter project to learn Dart and flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Container(
+    child: new Center(
+      child: new Text(
+        "Hai Flutter",
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  ));
+}
